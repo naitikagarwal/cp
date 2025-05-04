c468513b09dffc6c37146be91e5a96ad20add8f3
Naitik Agarwal <naitikagar12@gmail.com>
2025-05-04 18:24:35 +0530

    +1

--- C Set/371C.cpp
+++ C Set/371C.cpp
@@ -18,14 +18,14 @@ int main() {
         else if (ch=='C') c++;
         else if (ch=='S') s++;
     }
-    ll low=0, high =1e18, ans=0; // high > 1e12
+    ll low=0, high =1e13, ans=0; // high > 1e12
     while(low<=high){
         ll mid = (low + high)/2; // mid is targeted no. of burger
-        ll x1, x2, x3;// extra need
-        x1=max(0LL, b*mid-n1);
-        x2=max(0LL, s*mid-n2);
-        x3=max(0LL, c*mid-n3);
-        ll cost = x1*p1 + x2*p2 + x3*p3;
+        // ll x1, x2, x3;// extra need
+        ll x1=max(0LL, b * mid-n1);
+        ll x2=max(0LL, s * mid-n2);
+        ll x3=max(0LL, c * mid-n3);
+        ll cost = x1 * p1 + x2 * p2 + x3 * p3;
         
         if(cost<=r){
             ans = mid;

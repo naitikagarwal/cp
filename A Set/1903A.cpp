50fa05214f270c6dd9235060ef3320343be13c5d
Naitik Agarwal <naitikagar12@gmail.com>
2025-05-13 11:59:11 +0530

    1903A.cpp

---
+++ A Set/1903A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define rep(i , a , b) for(int i = (a) ; i <= (b) ; i++)
+#define per(i , a , b) for(int i = (a) ; i >= (b) ; i--)
+
+#define pb push_back
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.precision(10);
+    cout << fixed;
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vi a(n);
+        rep(i,0,n-1){
+            cin>>a[i];
+        }
+        int flag=0;
+        rep(i,0,n-2){
+            if (a[i]>a[i+1]) flag=1;
+        }
+        if(k<=1 && flag==1) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+    return 0;
+}

fdce56736c8e5feeee016a091f3a67a3c93f7842
Naitik Agarwal <naitikagar12@gmail.com>
2025-05-15 22:11:47 +0530

    1914C.cpp

---
+++ C Set/1914C.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define rep(i , a , b) for(int i = (a) ; i <= (b) ; i++)
+#define per(i , a , b) for(int i = (a) ; i >= (b) ; i--)
+
+#define pb push_back
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.precision(10);
+    cout << fixed;
+    int t; cin >> t;
+    while(t--){
+        int n,k;
+        cin >> n >> k;
+        vi a(n);
+        rep(i,0,n-1) cin >> a[i];
+        vi b(n);
+        rep(i,0,n-1) cin >> b[i];
+        int ans = 0;
+        int m=0, s=0;
+        for(int i=0; i<min(k,n); i++){
+            s += a[i];
+            m = max(m, b[i]);
+            ans = max(ans, s+(k-i-1)*m);
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
+}

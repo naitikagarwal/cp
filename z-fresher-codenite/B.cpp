605135c27f86102c59f011b142517a095430ee7c
Naitik Agarwal <naitikagar12@gmail.com>
2025-05-20 21:07:35 +0530

    codeclub

---
+++ z-fresher-codenite/B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define rep(i , a , b) for(int i = (a) ; i <= (b) ; i++)
+#define per(i , a , b) for(int i = (a) ; i >= (b) ; i--)
+
+#define pb push_back
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.precision(10);
+    cout << fixed;
+    int t;
+    cin >> t;
+    while(t--){
+        ll a,b,c;
+        cin >> a>>b>>c;
+        cout << min((a+b+c)/2, a+b) <<endl;
+
+    }
+    return 0;
+}

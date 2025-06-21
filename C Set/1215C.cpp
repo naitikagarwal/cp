e44b339540aec63d1154948ea4f6acbd1b3db48a
Naitik Agarwal <naitikagar12@gmail.com>
2025-06-21 11:30:58 +0530

    1215C

---
+++ C Set/1215C.cpp
@@ -0,0 +1,108 @@
+// Aur Bhai Dekhne aagaye ;)
+// Author: Naitik Agarwal
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("no-stack-protector")
+// #pragma GCC optimize("unroll-loops")
+// #pragma GCC target("sse,sse2,sse3,ssse3,popcnt,abm,mmx,tune=native")
+// #pragma GCC optimize("fast-math")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+#define pi (3.141592653589)
+#define mod 1000000007
+#define pb push_back
+#define is insert
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rfr(n) for (int i = n - 1; i >= 0; i--)
+#define rep(i, a, b) for (int i = a; i <= b; i++)
+#define per(i, a, b) for (int i = a; i >= b; i--)
+#define fr(n) for (int i = 0; i < n; i++)
+#define nesfr(x, y)             \
+    for (int i = 0; i < x; i++) \
+        for (int j = 0; j < y; j++)
+#define rep1(i, a, b) for (int i = a; i < b; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+typedef long long int ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define nl cout << "\n"
+const unsigned int M = 1000000007;
+const int N = 2e5 + 5;
+
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s, t;
+        cin >> s >> t;
+        int f[2] = {0};
+        rep(i, 0, n-1)
+        {
+            f[s[i] - 'a']++;
+            f[t[i] - 'a']++;
+        }
+
+        if (f[0] % 2 == 1 || f[1] % 2 == 1)
+        {
+            cout << -1 << '\n';
+        }
+        else
+        {
+            set<pair<char, int>> p;
+            fr(n)
+            {
+                if (s[i] == t[i])
+                    continue;
+                else
+                {
+                    p.insert({t[i], i});
+                }
+            }
+
+            vector<pair<int, int>> ans;
+            fr(n)
+            {
+                if (s[i] == t[i])
+                    continue;
+                auto it = p.upper_bound({t[i], i});
+                if (it == p.end() || it->first != t[i])
+                {
+                    it = p.lower_bound({t[i], i});
+                    p.erase(it);
+                    p.insert({s[i], i});
+                    swap(s[i], t[i]);
+                    ans.pb({i, i});
+                    i--;
+                }
+                else
+                {
+                    int j = it->second;
+                    std::swap(s[i], t[j]);
+                    p.erase(it);
+                    if (s[j] != t[j])
+                        p.insert({t[j], j});
+                    ans.pb({i, j});
+                }
+            }
+            cout << ans.size() << '\n';
+            for (auto it = ans.begin(); it != ans.end(); ++it)
+            {
+                cout << it->first + 1 << " " << it->second + 1 << '\n';
+            }
+        }
+    }
+    return 0;
+}

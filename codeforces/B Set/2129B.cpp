2a7a33c45bdfddf737fb7a82eb7ced2b471f084f
Naitik Agarwal <naitikagar12@gmail.com>
2025-08-04 20:47:18 +0530

    2129 2130 B

---
+++ codeforces/B Set/2129B.cpp
@@ -0,0 +1,58 @@
+// Aur Bhai Dekhne aagaye ;)
+// Author: Naitik Agarwal 
+
+#include <bits/stdc++.h>
+using namespace std;
+#define pi (3.141592653589)
+#define mod 1000000007
+#define pb push_back
+#define is insert
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rfr(n) for(int i=n-1;i>=0;i--)
+#define rep(i,a,b) for(long long i=a;i<=b;i++)
+#define per(i,a,b) for(long long i=a;i>=b;i--)
+#define fr(n) for(long long i=0;i<n;i++)
+#define nesfr(x,y) for(long long i=0;i<x;i++)for(long long j=0;j<y;j++)
+#define rep1(i,a,b) for(long long i=a;i<b;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+typedef long long int ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define nl cout << "\n"
+const unsigned int M = 1000000007;
+const int  N = 2e5 + 5 ;
+
+
+int main(){
+fast;
+ll t=1;
+cin>>t;
+while(t--)
+{
+    int n, ans=0;
+    cin >> n;
+    vll a(n);
+    fr(n){
+        cin>>a[i];
+    }
+
+    fr(n){
+        int l =0 , r=0;
+        rep(j,0, i-1){
+            if(a[j]>a[i]) l++;
+        }
+        rep(j, i+1,n-1){
+            if(a[j]>a[i])r++;
+        }
+        ans+= min(l, r);
+    }
+    cout<<ans;nl;
+}
+return 0;
+}

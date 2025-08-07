6fa14f80201fcad7e8b66bdbe8c811e056195450
Naitik Agarwal <naitikagar12@gmail.com>
2025-08-07 19:56:50 +0530

    2126C

---
+++ codeforces/C Set/2126C.cpp
@@ -0,0 +1,63 @@
+// Aur Bhai Dekhne aagaye ;)
+// Author: Naitik Agarwal 
+
+#include <bits/stdc++.h>
+using namespace std;
+#define pi (3.141592653589)
+#define mod 1000000007
+#define pb push_back
+#define is insert
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rfr(n) for(int i=n-1;i>=0;i--)
+#define rep(i,a,b) for(long long i=a;i<=b;i++)
+#define per(i,a,b) for(long long i=a;i>=b;i--)
+#define fr(n) for(long long i=0;i<n;i++)
+#define nesfr(x,y) for(long long i=0;i<x;i++)for(long long j=0;j<y;j++)
+#define rep1(i,a,b) for(long long i=a;i<b;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+typedef long long int ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define nl cout << "\n"
+const unsigned int M = 1000000007;
+const int  N = 2e5 + 5 ;
+
+
+int main(){
+fast;
+ll t=1;
+cin>>t;
+while(t--)
+{
+    int n,k;
+    cin>>n>>k;
+    vll a(n);
+    fr(n) cin>>a[i];
+    ll curr = a[k-1];
+    ll dist = a[k-1];
+
+    sort(a.begin(), a.end());
+    bool ans=true;
+
+    fr(n){
+        if(a[i]<curr) continue;
+        if(a[i] - curr>dist){
+            ans = false;
+            break;
+            
+        }
+        curr = a[i];
+    }
+
+    if(ans) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+
+}
+return 0;
+}
